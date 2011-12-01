46f9f150e92b150a09685a19ffaf289e0ce2c4d1
Rik Halfmouw <rik@iwg.nl>
2011-11-30 23:54:06 -0500

    Test code, not working yet

---
+++ lib/siriproxy-xbmc.rb
@@ -0,0 +1,48 @@
+# Copyright (C) 2011 by Rik Halfmouw <rik@iwg.nl>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+require 'cora'
+require 'siri_objects'
+require 'xbmc-client'
+
+#######
+# This is plugin to control XBMC
+# Remember to configure the host and port for your XBMC computer in config.yml in the SiriProxy dir
+######
+
+class SiriProxy::Plugin::XBMC < SiriProxy::Plugin
+  def initialize(config)
+    host = config["xbmc_host"]
+    port = config["xbmc_port"]
+    username = config["xbmc_username"]
+    password = config["xbmc_password"]
+    Xbmc.base_uri "http://" + host + ":" + port
+    Xbmc.basic_auth username, password
+    Xbmc.load_api!
+  end
+
+  #capture greeting and name, example: say hello to john
+  listen_for /play (.*)/i do |title|
+    say "Now playing \"#{title}\""
+    
+    request_completed #always complete your request! Otherwise the phone will "spin" at the user!
+  end
+  
+end
---
+++ siriproxy-xbmc.gemspec
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+$:.push File.expand_path("../lib", __FILE__)
+
+Gem::Specification.new do |s|
+  s.name        = "siriproxy-xbmc"
+  s.version     = "0.0.1" 
+  s.authors     = ["brainwave9"]
+  s.email       = [""]
+  s.homepage    = ""
+  s.summary     = %q{Siri Proxy Plugin to control XBMC}
+  s.description = %q{This is a plugin that allows you to control XBMC using Siri}
+
+  s.rubyforge_project = "SiriProxy-XBMC"
+
+  s.files         = `git ls-files 2> /dev/null`.split("\n")
+  s.test_files    = `git ls-files -- {test,spec,features}/* 2> /dev/null`.split("\n")
+  s.executables   = `git ls-files -- bin/* 2> /dev/null`.split("\n").map{ |f| File.basename(f) }
+  s.require_paths = ["lib"]
+
+  # specify any dependencies here; for example:
+  # s.add_development_dependency "rspec"
+  # s.add_runtime_dependency "rest-client"
+  s.add_runtime_dependency "xbmc-client"
+end
